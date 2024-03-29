8642a714a56c4b71fe80d81cba194beaa2773315
Piotr Prądzyński <prondzyn@gmail.com>
2024-03-29 07:14:57 +0100

    switch to snake_case strategy

--- src/main/java/software/hvb/blog/valuableendpointstests/users/User.java
+++ src/main/java/software/hvb/blog/valuableendpointstests/users/User.java
@@ -1,5 +1,9 @@
 package software.hvb.blog.valuableendpointstests.users;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(SnakeCaseStrategy.class)
 public record User (
         Long id,
         String firstName,
